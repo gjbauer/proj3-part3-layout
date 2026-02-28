7e542666ed84c6dd13037aee9a7be7cf074228af
Gabriel Bauer <gabeb1277@gmail.com>
2026-02-27 22:01:32 -0500

    implemented inode_write

--- inode.c
+++ inode.c
@@ -28,6 +28,21 @@ int inode_read(DiskInterface* disk, cache *cache, uint64_t inode_number, Inode* 
 
 int inode_write(DiskInterface* disk, cache *cache, const Inode* inode)
 {
+    Superblock sb;
+    superblock_read(disk, cache, &sb);
+    int inode_per_page = USABLE_BLOCK_SIZE / sizeof(Inode);
+    int inode_page = inode_number / inode_per_page;
+    block_type_t *block_type = get_block(disk, cache, 0, sb.inode_bitmap + calculate_inode_bitmap_size(&sb) + inode_page);
+    if (*block_type != BLOCK_TYPE_INODE)
+    {
+        fprintf(stderr, "ERROR: Not a valid inode table block!\n");
+        arc4random_buf(&sb, sizeof(struct Superblock));
+        return -1;
+    }
+    Inode *node = (Inode*) ( ( block_type + 1) + ( inode_number % inode_per_page ) );
+    memcpy(node, inode, sizeof(struct Inode));
+    arc4random_buf(&sb, sizeof(struct Superblock));
+    return 0;
 }
 
 uint64_t inode_allocate(DiskInterface* disk, cache *cache, FileType type)
